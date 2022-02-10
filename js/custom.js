9246ac493c33508efde1f7df5e9a16366774e745
njohligjin <njohligjin@gmail.com>
2022-02-10 23:49:32 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -338,7 +338,7 @@ $(document).ready(function(){
   } else if(window.location.href.indexOf("/shkaktimi-i-demit/")!=-1){
     $(".legal-reference").html("Bazuar në: <ul><li>Ligjin Nr. 7850, datë 29.07.1994, “Kodi Civil i Republikës së Shqipërisë”, i ndryshuar.</li><li>Ligjin Nr. 9109, datë 17.07.2003, “Për profesionin e avokatit në Republikën e Shqipërisë”, i ndryshuar.</li></ul>")
   }
-  else if(window.location.href.indexOf("siguria-ne-pune")!=-1){
+  else if(window.location.href.indexOf("/puna/siguria-ne-pune/")!=-1){
     $(".legal-reference").html("Bazuar në: <ul><li>Ligji Nr. 10237 “Për Sigurinë dhe Shëndetin në Punë“ datë 18.2.2010, i ndryshuar.</li><li>Ligjin Nr. 9109, datë 17.07.2003, “Për profesionin e avokatit në Republikën e Shqipërisë”, i ndryshuar.</li></ul>")
   }
 
